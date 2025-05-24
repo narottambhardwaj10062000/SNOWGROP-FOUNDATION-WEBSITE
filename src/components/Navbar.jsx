69990a52145ecb303a73b6d0663b04b4dc3a6ed1
narottambhardwaj10062000 <narottam.kumarbhardwaj.ece23@heritageit.edu.in>
2025-05-24 17:06:52 +0530

    changes done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { AppBar, IconButton, Toolbar } from '@mui/material'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <AppBar position='static' sx={{ bgcolor: "#384959" }}>
+        <Toolbar>
+
+            <IconButton>
+                
+            </IconButton>
+
+        </Toolbar>
+    </AppBar>
+
+  )
+}
+
+export default Navbar
