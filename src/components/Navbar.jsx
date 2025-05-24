0f70a8db07eb0f0535491aaf4759747b5a1bde32
Harsh-Kant <harshkant2020@gmail.com>
2025-05-24 19:00:35 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,55 @@
-import { AppBar, IconButton, Toolbar } from '@mui/material'
-import React from 'react'
+import { AppBar, IconButton, Toolbar, Box, Typography, List, ListItem,  } from '@mui/material'
+import React, { useState } from 'react'
+import NavBarLogo from "../assets/logo.png";
+import {theme} from "../App";
 
 const Navbar = () => {
-  return (
-    
-    <AppBar position='static' sx={{ bgcolor: "#384959" }}>
-        <Toolbar>
 
-            <IconButton>
-                
-            </IconButton>
+
+  let [links, setLinks] = useState(["About Us", "Our Team", "Our Campaigns", "Be a Volunteer", "Support Us"])
+
+
+  return (
+
+    <>
+    
+      <AppBar position='static' sx={{ bgcolor: theme.palette.background.default, height: "100px" }} >
+          <Toolbar>
+
+              <IconButton >
+                  <Box>
+                    <img src={NavBarLogo} style={{ width: "80px", height: "80px" }} />
+                  </Box>
+              </IconButton>
+
+              <Typography variant="h3" sx={{ ml: "24px", color: "#384959" }}>
+                SNOWDROP FOUNDATION
+              </Typography>
+
+          </Toolbar>
+      </AppBar>
+
+      <Box sx={{ width: "100vw", bgcolor: "#88BDF2" }}>
+
+        <Toolbar>
+        
+
+          {
+             links.map((link) => (
+                <ListItem key={link} sx={{ color: "black" }}>
+                  
+                    {link}
+                  
+                </ListItem>
+             ))
+          }
 
         </Toolbar>
-    </AppBar>
+        
+
+      </Box>
+
+    </>
 
   )
 }
